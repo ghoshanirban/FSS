6c301b5ec6145a6c4032881e094784bb3fbcb25f
Anirban Ghosh <39007826+ghoshanirban@users.noreply.github.com>
2025-03-09 14:40:59 -0400

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-To build the project, use CMake. Before running CMake, make sure that CGAL, Boost, GMP, MPFR are installed on your system. Please note that depending on your system, the supplied makefile CMakeLists.txt may need to be edited slightly. We have tested this project on Ubuntu 22.04 LTS. The libraries were installed using HomeBrew on our machines.
+To build the project, use CMake. Before running CMake, make sure that CGAL, Boost, GMP, MPFR are installed on your system. Please note that depending on your system, the supplied makefile CMakeLists.txt may need to be edited slightly. We have tested this project on Ubuntu 24.04 LTS. 
 
 Commands for building and running the project: 
 Prepare: cmake . 
--- main.cpp
+++ main.cpp
@@ -5,14 +5,15 @@
 #include "measurements/diameter.h"
 
 int main() {
-    unsigned n = 10E3; // set the number of points to be generated ( n > 1 )
+
     std::vector<Point> P;
 
     // choose a distribution and generate a random pointset
-    std::cout << timeStamp() << "Generating points randomly ... " << std::endl;
-    RandomPointGenerator g;
-    g.uni_square(n, 1000, P);
-    std::cout << timeStamp() << "Point generation is complete." << std::endl;
+    // unsigned n = 10E3; // set the number of points to be generated ( n > 1 )
+    // std::cout << timeStamp() << "Generating points randomly ... " << std::endl;
+    // RandomPointGenerator g;
+    // g.uni_square(n, 1000, P);
+    // std::cout << timeStamp() << "Point generation is complete." << std::endl;
 
     /*
      Other available generators:
@@ -25,18 +26,19 @@ int main() {
     */
 
     // Points can also be loaded from files
-    // readPointsFromFile("../RealWorldPointsets/burma.txt",P);
+    readPointsFromFile("RealWorldPointsets/monalisa.txt",P);
 
     std::vector<Edge> E; // every edge is represented using a pair of unsigned integers
     std::cout << timeStamp() << "Generating spanner ... " << std::endl;
     FSSEuclideanGraph G(P);
     bool mayHaveDuplicates = true; // If P can have duplicates, then 'mayHaveDuplicate' must be set to true
-    G.fastSparseSpanner(E, 1.1, mayHaveDuplicates, 1, false);
+    int threadCount = 4;
+    G.fastSparseSpanner(E, 1.1, mayHaveDuplicates, threadCount, false);
     std::cout << timeStamp() << "Spanner generation is complete. " << std::endl;
     std::cout << timeStamp() << "Computing stretch-factor ... " << std::endl;
-    std::cout << timeStamp() << "Stretch-factor: " <<  G.fastStretchFactor(4) << std::endl;
+    std::cout << timeStamp() << "Stretch-factor: " <<  G.fastStretchFactor(threadCount) << std::endl;
     std::cout << timeStamp() << "|E|: " << E.size() << std::endl;
-    std::cout << timeStamp() << "Avg. degree: " << (2.00 * (double)E.size()) / (double)P.size() << std::endl;
-    std::cout << timeStamp() << "Diameter: " << diameter(P, E, 4) << std::endl;
+    std::cout << timeStamp() << "Average degree: " << (2.00 * (double)E.size()) / (double)P.size() << std::endl;
+    std::cout << timeStamp() << "Diameter: " << diameter(P, E, threadCount) << std::endl;
     return 0;
 }
